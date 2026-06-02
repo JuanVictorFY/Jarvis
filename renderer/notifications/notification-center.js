2007d49b846366d04f0ffe89777b8e11b5f6d900
JuanVictorFY <figyuptonj99@gmail.com>
2026-06-02 03:47:14 -0500

    feat(notifications): add NotificationCenter component

---
+++ renderer/notifications/notification-center.js
@@ -0,0 +1,21 @@
+'use strict';
+class NotificationCenter {
+  constructor(container) {
+    this.container = container;
+    this.items = [];
+  }
+  add(n) {
+    this.items.unshift(n);
+    if (this.items.length > 50) this.items.pop();
+    this.render();
+  }
+  render() {
+    this.container.innerHTML = this.items.map(n =>
+      `<div class="notif notif--${n.type}">
+        <span class="notif-title">${n.title}</span>
+        <span class="notif-body">${n.body}</span>
+      </div>`
+    ).join('');
+  }
+}
+module.exports = NotificationCenter;
